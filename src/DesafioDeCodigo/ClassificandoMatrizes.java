73abc79f64cbf60f2f958e5e3b2621589e723ffb
Elise Melo <elisemelo@Elises-MacBook-Air.local>
2022-11-22 12:51:16 -0300

    Adicionando Desafio9 FatorialDesajeitado

--- src/DesafioDeCodigo/ClassificandoMatrizes.java
+++ src/DesafioDeCodigo/ClassificandoMatrizes.java
@@ -32,40 +32,39 @@ import java.util.*;
 import java.util.Scanner;
 
 public class ClassificandoMatrizes {
+    public static void main(String args[]) {
+        
+        Scanner input = new Scanner(System.in);
+         int i, aux;
+   	  //System.out.println("Digite o tamanho do Array: ");
+         int N = input.nextInt();
+       	int [] nums = new int[N];
 
-	 public static void main(String args[]) {
-         
-	     Scanner input = new Scanner(System.in);
-	      int i, aux;
-		  //System.out.println("Digite o tamanho do Array: ");
-	      int N = input.nextInt();
-	    	int [] nums = new int[N];
+       	for (i = 0; i < N; i++)
+       	{
+   			//System.out.println("Digite o " + (i+1) +"°" + " número: ");
+       		nums[i] = input.nextInt();
+       	}     	
+   		
+   		for (i = 0; i < N; i++)
+       	{
+   			if (nums[i] % 2 != 0){
 
-	    	for (i = 0; i < N; i++)
-	    	{
-				//System.out.println("Digite o " + (i+1) +"°" + " número: ");
-	    		nums[i] = input.nextInt();
-	    	}     	
-			
-			for (i = 0; i < N; i++)
-	    	{
-				if (nums[i] % 2 != 0){
-
-					for (int j = (i+1); j < N; j++)
-					{
-						if (nums[j] % 2 == 0) {
-							aux = nums[i];
-							nums[i] = nums[j];
-							nums[j] = aux;
-							j = N;
-						}					
-					}
-				}
-	    	}
-			
-			for (i = 0; i < N; i++)
-	    	{
-				System.out.println(nums[i]);
-	    	}  		
-	  	}
-	}
+   				for (int j = (i+1); j < N; j++)
+   				{
+   					if (nums[j] % 2 == 0) {
+   						aux = nums[i];
+   						nums[i] = nums[j];
+   						nums[j] = aux;
+   						j = N;
+   					}					
+   				}
+   			}
+       	}
+   		
+   		for (i = 0; i < N; i++)
+       	{
+   			System.out.println(nums[i]);
+       	}  		
+     	}
+   }
